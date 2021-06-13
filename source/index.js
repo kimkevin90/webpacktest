a0574b4a7294b51defb1242e4920c05de67558a3
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2021-06-13 10:17:52 +0900

    first commit

---
+++ source/index.js
@@ -0,0 +1,8 @@
+import hello_word from "./hello.js";
+import world_word from "./world.js";
+import css from "./styles.css";
+
+//css-loader는 css를 읽어와서 웹팩으로 가져옴
+//style-loader는 가져온것을 웹페이지안에 주입시킴
+document.querySelector("#root").innerHTML =
+  hello_word + world_word + world_word;
